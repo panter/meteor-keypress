3f44329558e7ea7f3757724f27a02dc15042af3f
Marco <macrozone@gmail.com>
2019-01-08 13:33:09 +0100

    remove coffee

--- package.js
+++ package.js
@@ -1,26 +1,24 @@
 Package.describe({
   name: 'panter:keypress',
-  version: '0.0.9',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'React to keypress - reactivly',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/panter/meteor-keypress',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
 Package.onUse(function(api) {
-  api.use('coffeescript', 'client');
-  api.use(['reactive-dict'], 'client');
-  api.versionsFrom('1.1.0.3');
-  api.export("Keypress", ["client"]);
-  api.addFiles('keypress.coffee', 'client');
+  api.use(['modules', 'ecmascript', 'reactive-dict'], 'client');
+  api.versionsFrom('1.5');
+  api.export('Keypress', ['client']);
+  api.addFiles('keypress.js', 'client');
 });
 
-
 Package.onTest(function(api) {
-  api.use('tinytest');
+  api.use(['modules', 'ecmascript', 'tinytest']);
   api.use('jquery');
   api.use('panter:keypress');
   api.addFiles('keypress-tests.js', 'client');
